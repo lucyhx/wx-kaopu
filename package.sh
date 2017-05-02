e30f9c54df9bfab77719df25d4476a5bc109417d
lucyhx <lucyhx@users.noreply.github.com>
2017-05-02 15:05:30 +0800

    update sh

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-#gitpacker zip wx-kaopu.zip . -e package.sh -e kao铺.zip -e *.map
-zip kao铺-hx.zip *.html image/* css/*.css
+gitpacker zip kao铺-hx.zip . -e '*.sh' -e '*.zip' -e '*.map' -e 'css'
+#zip kao铺-hx.zip *.html image/* css/*.css
